0c608b42ced6f74dc0c1ca70b4fb7ed948db9371
Leandro20001 <leandroferreira.dev@gmail.com>
2023-10-09 14:24:50 -0300

    Atualizando

--- main.js
+++ main.js
@@ -1,50 +1,41 @@
-window.sr = ScrollReveal({reset:false})
-sr.reveal('.mainText'),{duration:12000}
-
-const mainButton = document.querySelector('#mainButton')
-const mainSpan = document.querySelector('#mainSpan')
-//stage1//
-
-//requirindo os elementos do container//
-const h1 = document.querySelector('#secondSpan')
-const infoContainer = document.querySelector('#infoContainer');
-const infoContainer2 = document.querySelector('#infoContainer2')
-
-//funcao de slider//
-var img = 0;
-var allImg = [];
-var imgTimer = 5000;
-//selecionando todas as imagens///////////////////////////
 
 
-const mainImg = document.querySelector('#firstDisplay img')
-const samsungImg = document.getElementById('samsungImg')
-const mainImgContainer = document.querySelector('#mainImgContainer')
+const background = document.body
+const span = document.querySelector('#bigSpan')
+const $b = document.querySelectorAll('b')
 
-
-
-
-//botoes de mudanca de layout//
-const samsungButton = document.getElementById('samsungButton')
-const dellButton = document.querySelector('#dellButton')
-
-
-//ouvindo o clique do botao para executar as funcoes//
-samsungButton.addEventListener('click',(e)=>{
-  mainImg.classList.add('hide')
-  samsungImg.classList.remove('hide')
-  document.body.style.backgroundColor = '#101010'
+const buttons = document.querySelectorAll('#changeProduct button')
+buttons[0].addEventListener('click',(e)=>{
+  background.style.backgroundColor='white'
+  span.style.color='black'
+  $b[0].style.backgroundColor='black'
+  $b[0].style.color='aliceBlue'
+  $b[1].style.backgroundColor='black'
+  $b[1].style.color='aliceBlue'
+  e.stopPropagation()
+})
+buttons[1].addEventListener('click',(e)=>{
+  background.style.backgroundColor='black'
+  span.style.color='aliceBlue'
+  $b[0].style.backgroundColor='aliceBlue'
+  $b[0].style.color='black'
+  $b[0].style.boxShadow='0 0 14px aliceBlue'
+  $b[1].style.backgroundColor='aliceBlue'
+  $b[1].style.color='black'
+  $b[1].style.boxShadow='0 0 14px aliceBlue'
+  e.stopPropagation()
+})
+buttons[2].addEventListener('click',(e)=>{
+  background.style.backgroundColor='grey'
+  span.style.color='black'
+  $b[0].style.backgroundColor='black'
+  $b[0].style.color='aliceBlue'
+  $b[0].style.boxShadow='0 0 8px black'
+  $b[1].style.backgroundColor='black'
+  $b[1].style.color='aliceBlue'
+  $b[1].style.boxShadow='0 0 8px black'
   e.stopPropagation()
 })
 
-dellButton.addEventListener('click',(e)=>{
-  samsungImg.classList.add('hide')
-  mainImg.classList.remove('hide')
-  document.body.style.backgroundColor = 'white'
-  e.stopPropagation()
-})
+console.log(span)
 
-console.log(mainImg)
-console.log(samsungButton)
-console.log(mainImgContainer)
-console.log(samsungImg)
